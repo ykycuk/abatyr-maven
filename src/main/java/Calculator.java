7694c26584896c5e1a77b8e23bb81fd514cb2d3f
Artem Batyr <artembatyr88@gmail.com>
2021-10-07 20:50:40 +0300

    third commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,4 +2,12 @@ public class Calculator {
     static public int sum(int a, int b) {
         return a + b;
     }
+
+    static public int mult(int a, int b) {
+        return a * b;
+    }
+
+    static public double divide(int a, int b) {
+        return a / b;
+    }
 }
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -6,4 +6,11 @@ public class TestCalculator {
      public void test_sum_normal_case() {
       Assertions.assertEquals(4, Calculator.sum(3, 1));
     }
+
+
+
+    @Test
+    public void test_division_by_zero() {
+        Assertions.assertThrows(ArithmeticException.class, ()-> Calculator.divide(5, 0));
+    }
 }
